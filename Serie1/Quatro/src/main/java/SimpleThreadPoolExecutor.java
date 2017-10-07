c3f69abb51c6220bff6b6e89a17e128cdb5acbdb
DPDias <dpdias97@gmail.com>
2017-10-07 22:48:47 +0100

    Continuação do 4

--- Serie1/Quatro/src/main/java/SimpleThreadPoolExecutor.java
+++ Serie1/Quatro/src/main/java/SimpleThreadPoolExecutor.java
@@ -1,6 +1,13 @@
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class SimpleThreadPoolExecutor{
 
-    private final NodeLinkedList<Thread> threadsList = new NodeLinkedList<Thread>();
+    private final NodeLinkedList<ThreadContentor> threadsList = new NodeLinkedList<>();
+    private final NodeLinkedList<Runnable> tasks = new NodeLinkedList<>();
+    private final Lock lock = new ReentrantLock();
     private final int maxPoolSize;
     private final int keepAliveTime;
     private int numberOfThreads;
@@ -12,14 +19,82 @@ public class SimpleThreadPoolExecutor{
     }
 
     public boolean execute(Runnable command, int timeout) throws InterruptedException{
-        if(numberOfThreads < maxPoolSize){
-            Thread run = new Thread(command);
-            threadsList.push(run);
+        lock.lock();
+        try {
 
+            long t = Timeouts.start(timeout);
+            long remaining = Timeouts.remaining(t);
+
+            if (numberOfThreads < maxPoolSize) {
+                NodeLinkedList.Node<ThreadContentor> node = threadsList.push(new ThreadContentor(null, true, lock.newCondition()));
+                Thread run = new Thread(() -> threadWork(command, node));
+                node.value.thread = run;
+                return true;
+            }
+
+            while (true) {
+
+
+                //Check if there is one thread blocked but ready to work
+                //two lists or search in this list?????
+
+                Condition condition = lock.newCondition();
+                condition.await(remaining, TimeUnit.MILLISECONDS);  //lanço a exceção
+
+                remaining = Timeouts.remaining(t);
+                if(Timeouts.isTimeout(remaining)) {
+                    return false;
+                }
+            }
+        }finally{
+            lock.unlock();
         }
-
-        return false;
     }
+
+    public void threadWork(Runnable command, NodeLinkedList.Node<ThreadContentor> node){
+        command.run();
+        boolean timeout = false;
+        lock.lock();
+        try {
+            while (true) {
+
+                if (!tasks.isEmpty() && !timeout) {
+                    NodeLinkedList.Node<Runnable> run = tasks.pull();
+                    lock.unlock();
+                    timeout = true;
+                    run.value.run();        //poderá dar exceção?
+                    lock.lock();
+                }
+                else {
+                    try {
+                        node.value.condition.await(keepAliveTime, TimeUnit.MILLISECONDS);  //tratar timer
+                    } catch (InterruptedException e) {
+                        timeout = false;
+                    }
+                    if (timeout) {
+                        //tempo
+
+                    }
+                }
+            }
+        }
+        finally{
+            lock.unlock();  //poderá não ter o lock???
+        }
+    }
+
+    public static class ThreadContentor{
+        public Thread thread;
+        public boolean running;
+        public final Condition condition;
+
+        public ThreadContentor(Thread thread, boolean running, Condition condition) {
+            this.thread = thread;
+            this.condition = condition;
+            this.running = running;
+        }
+    }
+
 
 
 }
