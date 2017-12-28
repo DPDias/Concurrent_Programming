6532e81874632c3e1682b1e25eb4ca22f055d8bf
DPDias <dpdias97@gmail.com>
2017-12-28 16:43:02 +0000

    Realização do exercicio 4

---
+++ Serie3/DotNet/DotNet/AplicationSearchFiles/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace AplicationSearchFiles {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("");
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(770, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 301);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem2});
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(863, 198);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "File Path";
+            this.columnHeader1.Width = 732;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Size";
+            this.columnHeader2.Width = 126;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 216);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(863, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Write here directory path";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 242);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(863, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Number of files to show";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(352, 283);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(176, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(285, 286);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Files Found";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 363);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Search the Biggest Files";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Serie3/DotNet/DotNet/AplicationSearchFiles/Form1.cs
@@ -0,0 +1,83 @@
+﻿using fileSearch;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplicationSearchFiles {
+    public partial class Form1 : Form {
+        private BiggerFiles bf;
+        private CancellationTokenSource cts;
+
+        public Form1() {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if(cts != null)
+                cts.Cancel();
+        }
+
+        private async void button2_Click(object sender, EventArgs e) {
+            if(String.IsNullOrEmpty(textBox1.Text))
+                MessageBox.Show("Insira o path na caixa de texto ao lado da label 'dir path'");
+
+            listView1.Items.Clear();
+                     
+            cts = new CancellationTokenSource();
+            bf = new BiggerFiles(textBox1.Text, int.Parse(textBox2.Text), cts.Token);
+
+            button2.Enabled = false;
+            button1.Enabled = true;
+
+            Task searching = bf.Start();
+           
+            while(!searching.IsCanceled && !searching.IsCompleted && !searching.IsFaulted) {
+                FileInfo[] aux1 = bf.getBiggerFiles();
+                foreach(var a in aux1) {
+                    if (a != null) {
+                        ListViewItem item1 = new ListViewItem(a.FullName);
+                        item1.SubItems.Add("" + a.Length);
+                        listView1.Items.Add(item1);
+                    }
+                }
+
+                await Task.Delay(50).ConfigureAwait(true);
+                listView1.Items.Clear();
+            }
+
+            listView1.Items.Clear();
+                   
+            FileInfo[] aux = bf.getBiggerFiles();
+            foreach (var a in aux) {
+                ListViewItem item1 = new ListViewItem(a.FullName);
+                item1.SubItems.Add("" + a.Length);
+                listView1.Items.Add(item1);
+            }
+            textBox3.Text = "" + bf.GetNumberOfFiles();
+
+            button2.Enabled = true;
+            button1.Enabled = false;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+    }
+}
