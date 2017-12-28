44ae1c4842cf32a56df614721e197ce4e56ff2fd
DPDias <dpdias97@gmail.com>
2017-12-28 14:09:44 +0000

    Realização do exercicio 3

---
+++ Serie3/DotNet/DotNet/fileSearch/BiggerFiles.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace fileSearch {
+    public class BiggerFiles {
+        private String dirPath;
+        private FileInfo [] maxFiles;
+        private volatile int numberOfFiles;
+        private Object mon;
+        private Comparer<FileInfo> com;
+        private int idx = 0;
+        private CancellationToken ct;
+
+        public BiggerFiles(String dirPath, int maxFiles, CancellationToken ct) {
+            this.dirPath = dirPath;
+            this.maxFiles = new FileInfo [maxFiles];
+            com = Comparer<FileInfo>.Create((b, a) => (int)(a.Length - b.Length));
+            numberOfFiles = 0;
+            mon = new Object();
+            this.ct = ct;
+        }
+
+        public Task Start() {
+            return SearchFiles(Task.Run(() => new DirectoryInfo(dirPath)));
+        }
+
+        private async Task SearchFiles(Task<DirectoryInfo> dir) {
+            if (ct.IsCancellationRequested)
+                return;
+            DirectoryInfo di = await dir;
+            FileInfo[] allFiles = await Task.Run( () => di.GetFiles());    
+            DirectoryInfo[] allDirs = await Task.Run( () => di.GetDirectories());
+
+            Interlocked.Add(ref numberOfFiles, allFiles.Length);
+     
+            Parallel.For(0, allFiles.Length, (i) => {
+                if (ct.IsCancellationRequested)
+                    return;
+                if (idx < maxFiles.Length) {
+                    lock (mon) {
+                        if (idx < maxFiles.Length) {
+                            maxFiles[idx++] = allFiles[i];
+                            if (idx == maxFiles.Length)
+                                BuildMaxHeap(maxFiles, idx, com);
+                        } else {
+                            if (maxFiles[0].Length < allFiles[i].Length) {
+                                maxFiles[0] = allFiles[i];
+                            }
+                        }
+                    }
+                } else {
+                    lock (mon) {
+                        if (maxFiles[0].Length < allFiles[i].Length) {
+                            maxFiles[0] = allFiles[i];
+                        }
+                        maxHeapify(maxFiles, idx, com, 0);
+                    }
+                }
+                
+            });
+
+            foreach (DirectoryInfo aux in allDirs)
+                await SearchFiles(Task.Run(() => aux));
+
+        }
+
+        public int GetNumberOfFiles() {
+            return numberOfFiles;
+        }
+
+        public FileInfo [] getBiggerFiles() {
+            return maxFiles;
+        }
+
+        public static void BuildMaxHeap<T>(T[] a, int size, Comparer<T> cmp) {
+            for (int i = parent(size - 1); i >= 0; --i)
+                maxHeapify(a, size, cmp, i);
+        }
+
+
+        public static void maxHeapify<T>(T[] heap, int heapSize, Comparer<T> c, int i) {
+            int l = left(i);
+            int r = right(i);
+            int largest = (l >= heapSize || c.Compare(heap[l], heap[i]) <= 0) ? i : l;
+            if (r < heapSize && c.Compare(heap[largest], heap[r]) < 0) largest = r;
+            if (largest != i) {
+                swap(heap, i, largest);
+                maxHeapify(heap, heapSize, c, largest);
+                
+            }
+        }
+
+        public static int left(int i) {
+            return (i << 1) + 1;
+        }
+
+        public static int right(int i) {
+            return (i << 1) + 2;
+        }
+
+        public static int parent(int i) {
+            return (i - 1) >> 1;
+        }
+
+        public static void swap<T>(T[] a, int i1, int i2) {
+            T aux = a[i1];
+            a[i1] = a[i2];
+            a[i2] = aux;
+        }
+
+
+    }
+}
---
+++ Serie3/DotNet/DotNet/fileSearch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace fileSearch
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Escreva o caminho da pasta: ");
+            String dirPath = Console.ReadLine();
+            Console.WriteLine("Diga o numero de ficheiros a apresentar: ");
+            int maxFiles = int.Parse(Console.ReadLine());
+            CancellationTokenSource cts = new CancellationTokenSource();
+            var bg = new BiggerFiles(dirPath, maxFiles, cts.Token);
+            Task b = bg.Start();
+
+            Console.WriteLine("Deseja cancelar (s/n)?");
+            String answer = Console.ReadLine();
+            if (answer.Equals("s"))
+                cts.Cancel();          
+          
+
+            b.Wait();
+            Console.WriteLine(bg.GetNumberOfFiles());
+            /*
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            stopWatch.Stop();
+            long duration = stopWatch.ElapsedMilliseconds;*/
+            int a = 0;
+        }
+    }
+}
